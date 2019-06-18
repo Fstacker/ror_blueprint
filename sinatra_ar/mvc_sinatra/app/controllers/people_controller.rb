fc7947545ebbb903c7d63bbaafcfe32d50a6383e
fstacker <brett@yutster.com>
2019-06-17 21:12:54 -0600

    step 3 of 5: validate that data

--- sinatra_ar/mvc_sinatra/app/controllers/people_controller.rb
+++ sinatra_ar/mvc_sinatra/app/controllers/people_controller.rb
@@ -16,8 +16,22 @@ post '/people' do
 		birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
 	end
 
-	person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
-	redirect "/people/#{person.id}"
+	@person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+		if @person.valid?
+			@person.save
+			redirect "/people/#{@person.id}"
+		else
+			#@errors not published in SK solution
+			@person.errors.full_message.each do |msg| 
+				@errors = "#{@errors} #{msg}." 
+		end
+		erb :"people/new"
+	end
+end
+
+get '/people/:id/edit' do 
+	@person = Person.find(params[:id])
+	erb :'/people/edit'
 end
 
 get '/people/:id' do
@@ -28,32 +42,24 @@ get '/people/:id' do
 	erb :"/people/show"
 end
 
-get '/people/:id/edit' do 
-	@person = Person.find(params[:id])
-	erb :'/people/edit'
-end
-
 #block to update and edit a person's record
-put '/people/:id' do
-	person = Person.find(params[:id])
-	person.first_name = params[:first_name]
-	person.last_name = params[:last_name]
-	person.birthdate = params[:birthdate]
-	person.save
-	redirect "/people/#{person.id}"
-end
-
-=begin
-#method to update and edit a person's record
 put '/people/:id' do
 	@person = Person.find(params[:id])
 	@person.first_name = params[:first_name]
 	@person.last_name = params[:last_name]
 	@person.birthdate = params[:birthdate]
 	@person.save
-	redirect "/people/#{@person.id}"
+	#method to validate user entry; copied from the create a person method; Validations & Nice Looking Errors; step 3 of 5 
+	if @person.valid?
+		@person.save
+		redirect "/people/#{@person.id}"
+	else
+		@person.errors.full_message.each do |msg|
+			@errors = "#{@errors} #{msg}." 
+		end
+		erb :"people/edit"
+	end
 end
-=end
 
 # delete a record and route back to the list of people
 delete '/people/:id' do 
@@ -61,15 +67,3 @@ delete '/people/:id' do
 	person.delete
 	redirect "/people"
 end
-
-
-=begin
-#retrieve the birthpath number of the person from the Person class and convert the date to a readable string
-get '/people/:id' do 
-	@person = Person.find(params[:id])
-	birthdate_string = @person.birthdate.strftime("%m%d%y")
-	birth_path_num = Person.get_birth_path_num(birthdate_string)
-	@message = Person.get_message(birth_path_num)
-	erb :"/people/show"
-end
-=end
